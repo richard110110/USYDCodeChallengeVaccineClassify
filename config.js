ac451585b5ebfff56a048ca892805e8f2fc033b9
GUO CHENXI <guochenxi@MacBook-Pro-3.local>
2020-08-03 17:35:54 +1000

    upload

---
+++ config.js
@@ -0,0 +1,7 @@
+(function () {
+    "use strict";
+    exports.consumer_key = process.env.TWITTER_CONSUMER_KEY || 'vvets3BGqZrKVC7u4eSNUAKYB';
+    exports.consumer_secret = process.env.TWITTER_CONSUMER_SECRET || 'R5Oxpo5j9oruHpkt74spMdHrqIhUQtsFg1DOMpIX3ARuvebU8E';
+    exports.access_token_key = process.env.TWITTER_ACCESS_TOKEN_KEY || '805235363778207744-uueFrnKhDRlXC7r4b0pEUbHIbCQqSJL';
+    exports.access_token_secret =  process.env.TWITTER_ACCESS_TOKEN_SECRET || '3WD4U6l7SDqw3C4W6AuwQVE7fhHs4WLHS08UYOOHmk89w';
+})();
